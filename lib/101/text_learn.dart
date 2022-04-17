5575a72f5955af4061b151a230ef032a5a1ab1d5
erdemgocen44 <erdemgocen44@gmail.com>
2022-04-17 18:41:29 +0200

    başlangıç text

--- lib/101/text_learn.dart
+++ lib/101/text_learn.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
-class TextLearn extends StatelessWidget {
-  const TextLearn({Key? key}) : super(key: key);
-
+class TextLearnView extends StatelessWidget {
+  const TextLearnView({Key? key, required this.title}) : super(key: key);
+  final String title;
   @override
   Widget build(BuildContext context) {
     return Scaffold();
