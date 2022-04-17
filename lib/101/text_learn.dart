120744af7b5b302a0ebada4e20482b44eeaf65a4
erdemgocen44 <erdemgocen44@gmail.com>
2022-04-17 18:40:36 +0200

    başlangıç text

---
+++ lib/101/text_learn.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TextLearn extends StatelessWidget {
+  const TextLearn({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
