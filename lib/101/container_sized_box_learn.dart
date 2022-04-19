637223468874da3c1ea81fd0c8171ad3e992ff45
erdemgocen44 <erdemgocen44@gmail.com>
2022-04-19 19:43:29 +0200

    sized box ok

--- lib/101/container_sized_box_learn.dart
+++ lib/101/container_sized_box_learn.dart
@@ -17,7 +17,7 @@ class ContainerSizedBoxLearn extends StatelessWidget {
           const SizedBox
               .shrink(), //ekranda sadece boş bir alan oluşturmak istediğimizde kullanıyoruz
           SizedBox.square(
-            dimension: 50, //50*50lik bir kare yapar
+            dimension: 50, //50*50lik bir kare yapar.
             child: Text("ss" * 50),
           ), //bu kullanımda sadec kare bir alan istediğimizde yapıyoruz
           Container(),
