ac1844324af0ceb4463f59c7af2e0dd8610ee2c9
erdemgocen44 <erdemgocen44@gmail.com>
2022-04-23 19:07:52 +0200

    container starting

--- lib/101/container_sized_box_learn.dart
+++ lib/101/container_sized_box_learn.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class ContainerSizedBoxLearn extends StatelessWidget {
+  const ContainerSizedBoxLearn({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
