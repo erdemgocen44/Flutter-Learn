6354e75dbf60d9c9208e305eaa2610586b2fce8a
erdemgocen44 <erdemgocen44@gmail.com>
2022-04-24 19:45:30 +0200

    buttons starting

--- lib/101/button_learn.dart
+++ lib/101/button_learn.dart
@@ -7,14 +7,21 @@ class ButtonLearnView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Column(
-        children: [
-          TextButton(
-            child: const Text("Text Button"),
-            onPressed: () {},
-          ),
-          ElevatedButton(onPressed: () {}, child: Text('Elevated Button'))
-        ],
+      body: Center(
+        child: Column(
+          children: [
+            TextButton(
+              child: const Text("Text Button"),
+              onPressed: () {},
+            ),
+            ElevatedButton(onPressed: () {}, child: Text('Elevated Button')),
+            IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.chrome_reader_mode_outlined)),
+            FloatingActionButton(
+                onPressed: () {}, child: const Icon(Icons.add)),
+          ],
+        ),
       ),
     );
   }
